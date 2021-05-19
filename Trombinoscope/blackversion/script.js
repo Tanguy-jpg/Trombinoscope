65edb94a2caa71169007dce7434700b818edfc72
Tanguy-jpg <TanguyCarnot@gmail.com>
2021-05-19 20:10:36 +0200

    ajout des images de chaque personne. clic sur les avatars fonctionnels.

--- Trombinoscope/blackversion/script.js
+++ Trombinoscope/blackversion/script.js
@@ -11,7 +11,7 @@ function afficherImage( id)
 					node.id = id + "Image";
 					node.src = hrefImg;
 					node.alt = id;
-                    node.style = "width: 40%; min-width:470px; margin-left: -20%; height: 98%; margin-top: 10px; position:absolute; top: 0px; left: 50%; z-index:1000;"
+                    node.style = "width: 600px; min-width:470px; margin-left: -300px; height: 650px; position:absolute; bottom: 140px; left: 50%; z-index:1000;"
 
 					divImage.appendChild(node);
 				}
@@ -29,13 +29,67 @@ toggleBtn.addEventListener("click", function(){
 
 const toggleBtn = document.getElementById("toggleBtn")
 const content = document.getElementById("affichageImage")
-const avatar = document.getElementById("left", "test")
+
+const imageA = document.getElementById("imageA")
+const imageB = document.getElementById("imageB")
+const imageC = document.getElementById("imageC")
+const imageD = document.getElementById("imageD")
+const imageE = document.getElementById("imageE")
+const imageF = document.getElementById("imageF")
+const imageG = document.getElementById("imageG")
+const imageH = document.getElementById("imageH")
+const imageI = document.getElementById("imageI")
+const imageJ = document.getElementById("imageJ")
+const imageK = document.getElementById("imageK")
+const imageL = document.getElementById("imageL")
+const imageM = document.getElementById("imageM")
+const imageN = document.getElementById("imageN")
 
 toggleBtn.addEventListener("click", function(){
 	content.classList.toggle("affichageImage")
 })
 
 
-avatar.addEventListener("click", function(){
+imageA.addEventListener("click", function(){
 	content.classList.toggle("affichageImage")
 })
+imageB.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageC.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageD.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageE.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageF.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageG.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageH.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageI.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageJ.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageK.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageL.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageM.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+imageN.addEventListener("click", function(){
+	content.classList.toggle("affichageImage")
+})
+
